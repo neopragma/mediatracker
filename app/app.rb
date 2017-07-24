8e1c969488605289fa8bda135e7399c4c0192627
Dave Nicolette <davenicolette@gmail.com>
2017-07-24 06:38:01 -0700

    Refactored app/app.rb to simplify

--- app/app.rb
+++ app/app.rb
@@ -6,16 +6,19 @@ require_relative "./search"
 $config = YAML.load_file("./config/config.yml")
 
 get '/recordings/by/composer/:surname/:given_name' do
-  content_type :json
-  @search ||= Search.new
   return_as_json(
-    @search.find_recordings_by_composer({
+    search.find_recordings_by_composer({
       :surname => CGI.unescape(params[:surname]),
       :given_name => CGI.unescape(params[:given_name])})
-    )
+  )
+end
+
+def search
+  @search ||= Search.new
 end
 
 def return_as_json result
+  content_type :json
   {
     :request_url => "#{request.url}",
     :result => result
